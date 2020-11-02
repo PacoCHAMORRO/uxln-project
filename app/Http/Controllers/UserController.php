5822eac7efe711c1b1505db58fbea53901ff75ba
PacoCHAMORRO <pacochamfi@gmail.com>
2020-11-01 19:40:08 -0600

    Add implicit model binding to User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends ApiController
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(User $user)
     {
         $users = User::all();
 
@@ -51,13 +51,11 @@ class UserController extends ApiController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
-
         return $this->showOne($user);
     }
 
@@ -65,13 +63,11 @@ class UserController extends ApiController
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $user)
     {
-        $user = User::findOrFail($id);
-
         $rules = [
             'email' => 'email|unique:users,email,' .$user->id,
             'passwords' => 'min:7|confirmed',
@@ -111,13 +107,11 @@ class UserController extends ApiController
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
-
         $user->delete();
 
         return $this->showOne($user);
